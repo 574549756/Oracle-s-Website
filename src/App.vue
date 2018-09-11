54070622faf0efbf4805d33a105654ea1b8e271b
574549756 <574549756@qq.com>
2018-09-11 22:29:36 +0800

    css

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,36 @@
 <template>
   <div id="app">
     <div class="container">
-      
+      <topbar/>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+import topbar from './components/topbar'
+  export default {
+    name: 'App',
+    components:{
+      topbar
+    }
+  }
 </script>
 
 <style>
+.icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/topbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-gerenlogoBold"></use>
+        </svg>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+div {
+    margin: 2px;
+}
+</style>
