51d9a1cf9bf1c72fb1fbd896c0feed1114925781
Oracle <574549756@qq.com>
2018-11-05 16:56:51 +0800

    reset project

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,44 @@
 <template>
   <div id="app">
     <div class="container">
-      <topBar/>
-      <main>
-        <resumeBoard/>
-        <div class="detail"></div>  
-      </main>
+      <resumeBoard/>
     </div>
   </div>
 </template>
 
 <script>
-import resumeBoard from './components/resumeBoard'
-import topBar from './components/topBar'
-  export default {
-    name: 'App',
-    components:{
-      topBar,
-      resumeBoard
-    }
-  }
+import resumeBoard from "./components/resumeBoard"
+import topBar from "./components/topBar"
+export default {
+	name: "App",
+	components: {
+		resumeBoard
+	}
+}
 </script>
 
 <style lang="scss">
 .icon {
-    width: 1em;
-    height: 1em;
-    vertical-align: -0.15em;
-    fill: currentColor;
-    overflow: hidden;
+	width: 1em;
+	height: 1em;
+	vertical-align: -0.15em;
+	fill: currentColor;
+	overflow: hidden;
 }
 p {
-    color: #d2cfce;
-    font-weight: 600;
+	color: #d2cfce;
+	font-weight: 600;
 }
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    background-color: #3b3b3b;
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    .container {
-        width: 76vw;
-        display: flex;
-        background-color: #3b3b3b;
-        justify-content: center;
-        flex-direction: column;
-    }
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	background: url("./assets/Rose2kTiny.jpg") no-repeat center;
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 }
 </style>
--- src/components/resumeBoard.vue
+++ src/components/resumeBoard.vue
@@ -1,64 +1,34 @@
 <template>
-    <div class="nameBoard">
-        <main class="Id">
-            <header>
-                <h1>张博涵</h1>
-                <h2>前端工程师</h2>
-            </header>
-            <ul>
-                <li>出生日期</li>
-                <li>年龄</li>
-                <li>城市</li>
-                <li>学历</li>
-                <li>专业</li>
-                <li>学校</li>
-            </ul>
-            <ul>
-                <li>1994.2.25</li>
-                <li>24</li>
-                <li>重庆</li>
-                <li>本科</li>
-                <li>建筑学</li>
-                <li>大连民族大学</li>
-            </ul>
-        </main>
-        <div class="photo"></div>
-
-        <footer class="connection"></footer>
-    </div>
+  <div class="nameBoard">
+    <main>
+      <section class="about"></section>
+      <section class="ability"></section>
+      <section class="project"></section>
+      <section class="blog"></section>
+    </main>
+  </div>
 </template>
 
 <style lang="scss">
-    .nameBoard {
-        height: 600px;
-        width: 76vw;
-        background: rgb(80, 80, 80);
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        flex-direction: row;
-        padding: 40px;
-        .photo {
-            height: 70px;
-            width: 70px;
-            border-radius: 35px;
-            background: red;
-        }
-        .Id {
-            display: flex;
-            justify-content: space-between;
-            flex-direction: row;
-            align-items: center;
-            width: 100%;
-            ul {
-                display: flex;
-                justify-content: flex-start;
-                align-items: flex-start;
-                flex-direction: column;
-                li {
-                    font-size: 15px;
-                }
-            }
-        }
-    }
+.nameBoard {
+	height: 100vh;
+	width: 100vw;
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	flex-direction: row;
+	main {
+		height: 100vh;
+		width: 100vw;
+		display: flex;
+		section {
+			width: 25vw;
+			height: 100vh;
+			background: rgba(0, 0, 0, 0.4);
+			&:hover {
+				background: rgba(0, 0, 0, 0.1);
+			}
+		}
+	}
+}
 </style>
